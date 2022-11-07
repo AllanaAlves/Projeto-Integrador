232014e64571aef87b390de9e9aa77bca2754858
Allana33 <allana.alves08@hotmail.com>
2022-11-06 21:32:20 -0300

    att final

--- assets/js/login.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.querySelector('.login_form');
-  loginForm.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    location.href = '/homeoficial.html'
-  });
-});
